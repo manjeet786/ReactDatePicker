14ee39ae0e3abb1f4339ab4cfaccb6028ed031bf
Manjeet Choudhary <manjeetchoudhary193@gmail.com>
2020-08-08 01:30:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <DatePickerComponent id="datepicker" placeholder="Enter date"/>;
     </div>
   );
 }
